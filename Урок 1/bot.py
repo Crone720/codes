92ce3348b57ecd6c2181ae8d2c9ced10bca40a8d
Crone720 <143301210+Crone720@users.noreply.github.com>
2023-09-23 00:13:29 +0300

    Update bot.py

--- Урок 1/bot.py
+++ Урок 1/bot.py
@@ -5,16 +5,16 @@ intents = disnake.Intents.default()
 intents.message_content = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix='j!', intents=intents)
+bot = commands.Bot(command_prefix='j!', intents=intents) # в '' вставляете свой префикс
 
 @bot.event
 async def on_ready():
-	print('YT - JomixCode')
+	print('YT - JomixCode') # print это то что будет выводится в консоль
 	print('Бот Активный')
 
 @bot.command()
 async def hello(ctx):
-	member = ctx.author
+	member = ctx.author # создали переменую что member это ctx.author
 	await ctx.reply(f'Привет {member.mention}!')
 
 bot.run('you-token')
